413dd86c8f25f3bce9ba083609f1f0d63f70451f
mkuritsu <mkuritsuu@gmail.com>
2026-04-19 08:54:10 +0100

    fixed paru installation

--- system/paru.sh
+++ system/paru.sh
@@ -5,6 +5,7 @@ install_packages rustup gcc
 rustup default stable
 rustup component add rust-analyzer
 
+rm -rf /tmp/paru-clone
 git clone https://aur.archlinux.org/paru.git /tmp/paru-clone
-makepkg -si -D /tmp/paru-clone
+makepkg -si --noconfirm --needed -D /tmp/paru-clone
 rm -rf /tmp/paru-clone
