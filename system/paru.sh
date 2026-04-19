8a383ace79310cff643033b601cb8db411b598e2
mkuritsu <mkuritsuu@gmail.com>
2026-04-19 08:41:05 +0100

    removed paru comment

--- system/paru.sh
+++ system/paru.sh
@@ -5,6 +5,6 @@ install_packages rustup gcc
 rustup default stable
 rustup component add rust-analyzer
 
-# git clone https://aur.archlinux.org/paru.git /tmp/paru-clone
-# makepkg -si -D /tmp/paru-clone
-# rm -rf /tmp/paru-clone
+git clone https://aur.archlinux.org/paru.git /tmp/paru-clone
+makepkg -si -D /tmp/paru-clone
+rm -rf /tmp/paru-clone
