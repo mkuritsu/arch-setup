9147fb6a39f34d52f1958ce673e316e501d53c38
mkuritsu <mkuritsuu@gmail.com>
2026-04-20 22:08:56 +0100

    removed vicinae, fixed paru check

--- apps/desktop.sh
+++ apps/desktop.sh
@@ -5,7 +5,6 @@ install_packages nautilus papers loupe file-roller
 install_packages keyd
 
 install_aur_packages helium-browser-bin
-install_aur_packages vicinae-bin
 install_aur_packages opentabletdriver
 
 systemctl enable --user --now opentabletdriver
--- system/paru.sh
+++ system/paru.sh
@@ -6,10 +6,10 @@ rustup default stable
 rustup component add rust-analyzer
 
 if command -v paru >/dev/null 2>&1; then
+    echo "Paru already installed, skipping."
+else
     rm -rf /tmp/paru-clone
     git clone https://aur.archlinux.org/paru.git /tmp/paru-clone
     makepkg -si --noconfirm --needed -D /tmp/paru-clone
     rm -rf /tmp/paru-clone
-else
-  echo "Paru already installed, skipping."
 fi
